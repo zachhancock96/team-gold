6aae29955b74df4f922f1381ae2e198b5e36af21
nataword <60165036+nataword@users.noreply.github.com>
2020-01-27 09:58:51 -0600

    Add files via upload

--- RepProfile.php
+++ RepProfile.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link rel="stylesheet" href="/css/style.css">
 	<title>Rep Profile</title>
 	<script src="jquery-3.4.1.min.js"></script>
@@ -10,6 +13,21 @@
 	<header>
 		<h1>Rep Profile</h1>
 	</header>
+	
+	<div>
+
+    <table>
+        <tr>
+        <td>+ Add Games +</td>
+        <td>+ Verify Games +</td>
+        <td>+ Profile +</td>  
+        </tr>
+    </table>
+    
+
+    </div>
+	
+	
 	Name:		John Doe<br>
 	School:		West Monroe High School<br>
 	Phone:		111-111-1111<br>
--- SchoolProfile.php
+++ SchoolProfile.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link rel="stylesheet" href="/css/style.css">
 	<title>School Profile</title>
 	<script src="jquery-3.4.1.min.js"></script>
@@ -10,6 +13,21 @@
 	<header>
 		<h1>School Profile</h1>
 	</header>
+	
+	<div>
+
+    <table>
+        <tr>
+        <td>+ Add Games +</td>
+        <td>+ Verify Games +</td>
+        <td>+ Profile +</td>  
+        </tr>
+    </table>
+    
+
+    </div>
+	
+	
 School Name:	West Monroe High School<br>
 LHSAA Names:	West Monroe High School VB<br>
 				West Monroe High School VG<br>
