15615443b5f5d6c677e0d562c82d185263c4a4fe
nataword <60165036+nataword@users.noreply.github.com>
2020-01-26 21:32:44 -0600

    Add files via upload

--- addGame.php
+++ addGame.php
@@ -1,10 +1,19 @@
 <!DOCTYPE html>
 <html>
-<body>
+<head>
+	<link rel="stylesheet" href="style.css">
+	<title>Add Game Confirmation</title>
+	<script src="jquery-3.4.1.min.js"></script>
+</head>
 
-You have added <?php echo $_POST["HSchool"]; ?> <?php echo $_POST["HTeam"]; ?> VS
-<?php echo $_POST["VSchool"]; ?> <?php echo $_POST["HTeam"]; ?>
-on <?php echo $_POST["date"]; ?> at <?php echo $_POST["location"]; ?>
+
+<body>
+	<header>
+		<h1>Add Game</h1>
+	</header>
+	You have added <?php echo $_POST["HSchool"]; ?> <?php echo $_POST["HTeam"]; ?> VS
+	<?php echo $_POST["VSchool"]; ?> <?php echo $_POST["HTeam"]; ?>
+	on <?php echo $_POST["date"]; ?> at <?php echo $_POST["location"]; ?>
 
 </body>
 </html>
