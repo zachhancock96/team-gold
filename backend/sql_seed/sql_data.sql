5f6b5380fba2072d0d17a2de111579daf068aa21
Natalie Ross <natalieross1986@yahoo.com>
2020-03-02 09:39:12 -0600

    update sql data for new tables

--- backend/sql_seed/sql_data.sql
+++ backend/sql_seed/sql_data.sql
@@ -1,18 +1,72 @@
-DELETE FROM Game;
-DELETE FROM Team;
-DELETE FROM School;
+DELETE FROM GAME;
+DELETE FROM TEAM;
+DELETE FROM SCHOOL;
+DELETE FROM ROLES;
+DELETE FROM TEAMS;
+DELETE FROM USER;
+DELETE FROM TEAM_CLASS;
+DELETE FROM GENDER;
+DELETE FROM DISTRICT;
+DELETE FROM REP;
+DELETE FROM ASSIGNOR;
+DELETE FROM SCHOOL_ADMIN;
 
-INSERT INTO School (id, name, abbrevName) VALUES
-  (1, 'Neville High School', 'Neville H.S'),
-  (2, 'Ginsburg High School', 'Ginsburg H.S'),
-  (3, 'West Monroe High School', 'West Monroe H.S');
+INSERT INTO TEAM_CLASS (id, name, abbrevName) VALUES
+  (1, 'Varsity', 'Var'),
+  (2, 'Junior Varsity', 'JV');
 
-INSERT INTO Team (id, name, abbrevName, schoolId) VALUES
-  (1, 'Neville High School Jr. Varsity', 'Neville H.S.J.V', 1),
-  (2, 'Ginsburg High School Jr. Varsity', 'Ginsburg H.S.J.V', 2),
-  (3, 'West Monroe High School Jr. Varsity', 'West Monroe H.S.J.V', 3);
+INSERT INTO GENDER (id, name, abbrevName) VALUES
+  (1, 'Female', 'F'),
+  (2, 'Male', 'M');
 
-INSERT INTO Game (id, homeTeamId, awayTeamId, start, location) VALUES
-  (1, 1, 2, '2020-03-15T08:00:00.000Z', 'Trump Tower, Monroe, LA, 71293'),
-  (2, 2, 1, '2020-03-16T08:00:00.000Z', 'Trump Tower, Monroe, LA, 71293'),
-  (3, 1, 3, '2020-03-17T08:00:00.000Z', 'Trump Tower, Monroe, LA, 71293');
+INSERT INTO SCHOOL (id, name, abbrevName, distId) VALUES
+  (1, 'Neville High School', 'Neville H.S.', 1),
+  (2, 'Ginsburg High School', 'Ginsburg H.S.', 2),
+  (3, 'West Monroe High School', 'West Monroe H.S.', 1);
+
+INSERT INTO TEAM (id, schoolId, classId, genId) VALUES
+  (1, 1, 1, 1),
+  (2, 1, 1, 2),
+  (3, 1, 2, 1),
+  (4, 1, 2, 2),
+  (5, 2, 1, 1),
+  (6, 2, 1, 2),
+  (7, 2, 2, 1),
+  (8, 2, 2, 2),
+  (9, 3, 1, 1),
+  (10, 3, 1, 2),
+  (11, 3, 2, 1),
+  (12, 3, 2, 2);
+
+INSERT INTO GAME (id, homeTeamId, awayTeamId, start, location, status) VALUES
+  (1, 1, 2, '2020-03-15T08:00:00-05:00', 'WOHS', 'pend_team'),
+  (2, 2, 1, '2020-03-16T08:00:00-05:00', 'WMHS', 'pend_team'),
+  (3, 1, 3, '2020-03-17T08:00:00-05:00', 'OPHS', 'pend_team');
+
+INSERT INTO ROLES (id, name) VALUES
+  (1, 'System Admin'),
+  (2, 'Assignor'),
+  (3, 'School Admin'),
+  (4, 'School Representative');
+
+INSERT INTO USER (email, lastSignedIn, fName, Lname, role, password) VALUES
+  ('systema@test.net', 2020-03-01, 'systema', 'systema', 1, 'systema'),
+  ('assignor@test.net', 2020-03-01, 'assignor', 'assignor', 2, 'assignor'),
+  ('schoola@test.net', 2020-03-01, 'schoola', 'schoola', 3, 'schoola'),
+  ('rep@test.net', 2020-03-01, 'rep', 'rep', 4, 'rep');
+
+INSERT INTO REP (email, schoolId) VALUES
+  ('rep@test.net', 1),
+  ('rep2@test.net', 2);
+
+INSERT INTO SCHOOL_ADMIN (email, schoolId) VALUES
+  ('schoola@test.net', 1),
+  ('schoola2@test.net', 2);
+
+INSERT INTO ASSIGNOR (email, districtId) VALUES
+  ('assignor@test.net', 1),
+  ('assignor2@test.net', 2);
+
+INSERT INTO DISTRICT (id, disName) VALUES
+  (1, 'Ouachita'),
+  (2, 'Morehouse');
--- backend/sql_seed/sql_table.sql
+++ backend/sql_seed/sql_table.sql
@@ -2,41 +2,119 @@ CREATE Database IF NOT EXISTS SoccerSchedule;
 
 USE SoccerSchedule;
 
-DROP TABLE IF EXISTS Game;
-DROP TABLE IF EXISTS Team;
-DROP TABLE IF EXISTS School;
+DROP TABLE IF EXISTS GAME;
+DROP TABLE IF EXISTS TEAM;
+DROP TABLE IF EXISTS SCHOOL;
+DROP TABLE IF EXISTS ROLES;
+DROP TABLE IF EXISTS TEAMS;
+DROP TABLE IF EXISTS USER;
+DROP TABLE IF EXISTS TEAM_CLASS;
+DROP TABLE IF EXISTS GENDER;
+DROP TABLE IF EXISTS DISTRICT;
+DROP TABLE IF EXISTS REP;
+DROP TABLE IF EXISTS ASSIGNOR;
+DROP TABLE IF EXISTS SCHOOL_ADMIN;
 
-CREATE TABLE School (
+CREATE TABLE SCHOOL (
   id int NOT NULL AUTO_INCREMENT,
   name varchar(55) NOT NULL,
   abbrevName varchar(55) NOT NULL,
+  distId int NOT NULL,
 
   PRIMARY KEY(id),
+  FOREIGN KEY(distId) REFERENCES DISTRICT(id),
   UNIQUE (name),
   UNIQUE (abbrevName)
 );
 
-CREATE TABLE Team (
+CREATE TABLE TEAM (
   id int NOT NULL AUTO_INCREMENT,
-  name varchar(55) NOT NULL,
-  abbrevName varchar(55) NOT NULL,
   schoolId int NOT NULL,
+  classId int NOT NULL,
+  genId int NOT NULL,
   
   PRIMARY KEY(id),
-  FOREIGN KEY(schoolId) REFERENCES School(id),
-  UNIQUE (name),
-  UNIQUE (abbrevName)
+  FOREIGN KEY(schoolId) REFERENCES SCHOOL(id),
+  FOREIGN KEY(classId) REFERENCES TEAM_CLASS(id),
+  FOREIGN KEY(genId) REFERENCES GENDER(id)
 );
 
-CREATE TABLE Game (
+CREATE TABLE GAME (
   id int NOT NULL AUTO_INCREMENT,
   homeTeamId int,
   awayTeamId int,
   start DateTime NOT NULL,
   location varchar(55),
+  status varchar(15),
 
   PRIMARY KEY(id),
-  FOREIGN KEY(homeTeamId) REFERENCES Team(id),
-  FOREIGN KEY(awayTeamId) REFERENCES Team(id)
+  FOREIGN KEY(homeTeamId) REFERENCES TEAM(id),
+  FOREIGN KEY(awayTeamId) REFERENCES TEAM(id)
 );
 
+CREATE TABLE TEAM_CLASS (
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(25) NOT NULL,
+  abbrevName varchar(10) NOT NULL
+);
+
+CREATE TABLE GENDER (
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(15),
+  abbrevName char,
+
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE ROLES (
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(40),
+
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE USER (
+  email varchar(50) NOT NULL,
+  lastSignedIn DateTime,
+  fName varchar(30),
+  lName varchar(30),
+  role int NOT NULL,
+  password varchar(30),
+
+  PRIMARY KEY(email),
+  FOREIGN KEY(role) REFERENCES ROLES(id)
+);
+
+CREATE TABLE REP (
+  email varchar(50) NOT NULL,
+  schoolId int,
+
+  PRIMARY KEY(email),
+  FOREIGN KEY(email) REFERENCES USER(email)
+  FOREIGN KEY(schoolId) REFERENCES SCHOOL(id)
+);
+
+CREATE TABLE SCHOOL_ADMIN (
+  email varchar(50) NOT NULL,
+  schoolId int NOT NULL,
+
+  PRIMARY KEY(email),
+  FOREIGN KEY(email) REFERENCES USER(email)
+  FOREIGN KEY(schoolId) REFERENCES SCHOOL(id)
+);
+
+CREATE TABLE ASSIGNOR (
+  email varchar(50) NOT NULL,
+  districtId int NOT NULL,
+
+  PRIMARY KEY(email),
+  FOREIGN KEY(email) REFERENCES USER(email)
+  FOREIGN KEY(districtId) REFERENCES DISTRICT(id)
+);
+
+CREATE TABLE DISTRICT (
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(30),
+
+  PRIMARY KEY(id)
+);
