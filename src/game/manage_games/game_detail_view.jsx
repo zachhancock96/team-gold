9e53f259e9f3c4973d0bd0f867e4133e3f27eaea
Abhaya Uprety <zzxoto@gmail.com>
2020-04-10 13:18:08 -0500

    xx

--- src/game/manage_games/game_detail_view.jsx
+++ src/game/manage_games/game_detail_view.jsx
@@ -11,36 +11,37 @@ import { prettyDate, prettyDateAndTime } from '../utils';
   NOTE: thiese onEdit, onReject, onAccept callback do nothing for now (might even throw error)
   once you are done, message me
 */
-export const GameDetail = ({ showLoading, gameDetail, onEdit, onReject, onAccept }) => {
-  if(gameDetail){
-    console.log(gameDetail.history[0]);
-    
-    const tempGameDetail = 
+export const GameDetail = ({ gameDetail, onEdit, onReject, onAccept }) => {
+  if (!gameDetail) return null;
+
+  console.log(gameDetail.history[0]);
+
+  const tempGameDetail =
     gameDetail.game.homeTeam.school.name + ' ' + gameDetail.game.homeTeam.teamKind + ' vs '
     + gameDetail.game.awayTeam.school.name + ' ' + gameDetail.game.awayTeam.teamKind;
 
-    const tempHistDetail = 
+  const tempHistDetail =
     gameDetail.game.homeTeam.school.name + ' ' + gameDetail.game.homeTeam.teamKind + ' vs '
     + gameDetail.game.awayTeam.school.name + ' ' + gameDetail.game.awayTeam.teamKind;
-      
 
-    return <div><div id='detailGame'><div id='gamedetail-heading'>Game</div>
+
+  return (
+    <div><div id='detailGame'><div id='gamedetail-heading'>Game</div>
       <div>{tempGameDetail}</div>
       <div>{prettyDateAndTime(gameDetail.game.start)}</div>
       <div>{gameDetail.game.location}</div>
-      </div>
+    </div>
       <div id='detailHistory'><div id='gamedetail-heading'>Game History</div>
-      <div id='timestamp'>{gameDetail.history[0].timestamp}</div>
-      <div>{tempHistDetail}</div>
-      <div>{prettyDateAndTime(gameDetail.history[0].start)}</div>
-      <div>{gameDetail.history[0].location}</div>
+        <div id='timestamp'>{gameDetail.history[0].timestamp}</div>
+        <div>{tempHistDetail}</div>
+        <div>{prettyDateAndTime(gameDetail.history[0].start)}</div>
+        <div>{gameDetail.history[0].location}</div>
       </div>
       <div id='detailStatus'><div id='gamedetail-heading'>Status</div>
-      <div>{gameDetail.game.status}</div>
+        <div>{gameDetail.game.status}</div>
       </div>
     </div>
-  }
-  else{return <div><pre>{JSON.stringify('', null, 2)}</pre></div>}
+  );
 }
 
 //use this to pretty history
@@ -48,15 +49,15 @@ export const GameDetail = ({ showLoading, gameDetail, onEdit, onReject, onAccept
 //@return Array<{timestamp: string, body: Array[string]}
 //console.log this value if not clear
 const prettyHistory = (history) => {
-  
+
   return history.map(h => {
     const by = h.updaterType === 'home'
       ? game.homeTeam.school.name
       : h.updaterType === 'away'
-      ? game.awayTeam.school.name
-      : h.updaterType === 'assignor'
-      ? 'Assignor'
-      : 'Admin';
+        ? game.awayTeam.school.name
+        : h.updaterType === 'assignor'
+          ? 'Assignor'
+          : 'Admin';
 
     const ret = body => ({
       timestamp: prettyDateAndTime(g.timestamp),
@@ -78,8 +79,8 @@ const prettyHistory = (history) => {
       const k = h.updaterType === 'assignor' || h.updaterType === 'admin'
         ? 'Approved'
         : 'Accepted';
-      
-      return ret([ `${k} by ${by}` ]);
+
+      return ret([`${k} by ${by}`]);
     } else {
       return ret([`Rejected by ${by}`]);
     }
--- src/game/manage_games/list_view.jsx
+++ src/game/manage_games/list_view.jsx
@@ -9,43 +9,40 @@ import { prettyDate, prettyDateAndTime } from '../utils';
 */
 export const ListView = ({ showLoading, games, onGameClick, activeGameId }) => {
 
-  const gameItems = games.map(g => 
-  <li style={{ 
-    padding: '10px',
-    cursor: 'pointer'
-   }} 
-   key={g.id}
-   onClick={() => { onGameClick(g.id)}}
-   >
-  {/* {JSON.stringify(g)} */}
+  const gameItems = games.map(g =>
+    <li style={{
+      padding: '10px',
+      cursor: 'pointer'
+    }}
+      key={g.id}
+      onClick={() => { onGameClick(g.id) }}
+    >
+      <div className="game-box">
 
+        <div className="contents">
+          <p>{g.homeTeam.school.name}<br />
+            {g.awayTeam.school.name}
 
-  <div class="game-box">
-        {/* <div class="color">
-
-        </div> */}
-
-          <div class="contents">
-              <p>{g.homeTeam.school.name}<br/>
-              {g.awayTeam.school.name}
-
-              <p class="school-type"><a class="font-before">Type: &emsp; &emsp;</a>{g.homeTeam.teamKind}</p>
-                  <a class="field">
-                  <a class="font-before">Location: &nbsp; </a> {g.location}
-                  </a>
-              </p>
-              <p class="time"><a class="font-before">Time: &emsp; &emsp;</a>{g.start}</p>
-              <p class="status"><a class="font-before">Status: &nbsp; &emsp;</a>{g.status}</p>
-          </div>
-    </div>
-  </li>
-
-
-)
-// games.map(game => <button onClick={() => onGameClick(game.id)}>{game.id}</button>))
+            <p className="school-type">
+              <a className="font-before">
+                Type: &emsp; &emsp;
+              </a>
+              {g.homeTeam.teamKind}
+            </p>
+            <p className="field">
+              <a className="font-before">Location: &nbsp;</a>
+              {g.location}
+            </p>
+          </p>
+          <p className="time"><a className="font-before">Time: &emsp; &emsp;</a>{g.start}</p>
+          <p className="status"><a className="font-before">Status: &nbsp; &emsp;</a>{g.status}</p>
+        </div>
+      </div>
+    </li>
+  );
 
   return <ul>{gameItems}</ul>
- 
+
 
 }
 
