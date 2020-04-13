19512a2d58fa303d2e1d0ff694868d4f16bc84d0
Natalie Ross <natalieross1986@yahoo.com>
2020-04-12 20:36:12 -0500

    edit to calendar

--- src/calendar/controller.jsx
+++ src/calendar/controller.jsx
@@ -1,6 +1,41 @@
-import React  from 'react';
+import React from 'react'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import '../../node_modules/@fullcalendar/core/main.css';
+import '../../node_modules/@fullcalendar/daygrid/main.css';
+import {api} from 'shared';
+import { calendar } from 'calendar';
 
-export const Calendar = ({state, actions, routing}) => {
-  return <div>Calendar (Don't worry about this)</div>
+
+export class Calendar extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      games : []
+    }
+  }
+
+  componentDidMount() {
+    api.getGames().then((games) => {
+      this.setState({games});
+    })
+  }
+
+  render() {
+    const games = this.state.games.map(g => ({
+      title: g.homeTeam.name  + ' vs ' + g.awayTeam.name + ' @ ' + g.location,
+      start: g.start,
+      end: g.start
+    }));
+
+    return (
+      <FullCalendar defaultView="dayGridMonth"
+      plugins={[ dayGridPlugin ]} 
+      events={games} />
+    )
+  }
+
 }
+
 Calendar.showNavbar = true;
